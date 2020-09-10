c5e2ada1f3cca91c44ac4c25ff058545a7666f35
tugudush <jerome2kph@gmail.com>
2020-09-11 00:56:53 +0800

    progress

--- vue-docs/05-class-and-style-bindings/index.js
+++ vue-docs/05-class-and-style-bindings/index.js
@@ -28,3 +28,60 @@ var app3 = new Vue({
     } // end of assObject: function()
   } // end of computed:
 }) // end of app3
+
+var app4 = new Vue({
+  el: '#app4',
+  data: {
+    activeClass: 'active',
+    errorClass: 'text-danger'
+  }
+}) // end of app4
+
+var app5 = new Vue({
+  el: '#app5',
+  data: {
+    isActive: true,
+    activeClass: 'active',
+    errorClass: 'text-danger'
+  }
+}) // end of app5
+
+Vue.component('my-component', {
+  template: '<p class="foo bar">Example 6 - With Components</p>'
+})
+
+var app6 = new Vue({
+  el: '#app6'
+})
+
+var app7 = new Vue({
+  el: '#app7',
+  data: {
+    activeColor: 'red',
+    fontSize: 30
+  }
+}) // end of app7
+
+var app7b = new Vue({
+  el: '#app7b',
+  data: {
+    styleObject: {
+      color: 'red',
+      fontSize: '13px'
+    }
+  }
+}) // end of app7b
+
+var app8 = new Vue({
+  el: '#app8',
+  data: {
+    baseStyles: {
+      'font-style': 'italic',
+      'font-size': '24px',
+      color: '#333'
+    },
+    overridingStyles: {
+      fontSize: '60px'
+    }
+  }  
+}) // end of app8
---
+++ vue-docs/06-conditional-rendering/index.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    awesome: true
+  }
+})
+
+var app2 = new Vue({
+  el: '#app2',
+  data: {
+    ok: true
+  }
+})
