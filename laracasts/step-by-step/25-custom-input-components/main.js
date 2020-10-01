53c904905b32cc41a8859d36e7e791df815f66a5
tugudush <jerome2kph@gmail.com>
2020-10-01 12:38:42 +0800

    custom input components

--- laracasts/step-by-step/25-custom-input-components/main.js
+++ laracasts/step-by-step/25-custom-input-components/main.js
@@ -1,6 +1,37 @@
+Vue.component('coupon', {
+  props: ['code'],
+
+  template: `
+    <input type="text" :value="code" @input="updateCode($event.target.value)" ref="input">
+  `,
+
+  data() {
+    return {
+      invalids: ['ALLFREE', 'SOMETHINGELSE']
+    }
+  },
+
+  methods: {
+
+    updateCode(code) {
+
+      // validation
+      if (this.invalids.includes(code)) {
+        alert('This coupon is no longer valid. Sorry!');
+        this.$refs.input.value = ''
+        return;
+      }
+
+      this.$emit('input', code);
+    } // end of updateCode(code),
+
+  }, // end of methods
+
+});
+
 var app = new Vue({
   el: '#app',
   data: {
-    coupon: 'FREEBIE'
-  } // end of data
-}) // end of app
+    coupon: 'FREEBIE',
+  }
+});
